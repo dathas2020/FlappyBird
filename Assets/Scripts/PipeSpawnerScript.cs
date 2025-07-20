6a229b8d5ca5fe8c5fd4591383e2392cc159a5c1
dathas2020 <dathas2020@gmail.com>
2025-07-20 14:14:59 +0530

    Added random pipe spawn rate
    pipes spawn with random times according to your score.

--- Assets/Scripts/PipeSpawnerScript.cs
+++ Assets/Scripts/PipeSpawnerScript.cs
@@ -6,11 +6,12 @@ public class PipeSpawnerScript : MonoBehaviour
     [SerializeField] private float spawnRate = 2.5f;
     private float timer;
     public Vector2 spawnRateRange = new Vector2(4f, 5f);
+    public LogicScript logic;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        logic = GameObject.FindGameObjectWithTag("logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
@@ -36,7 +37,22 @@ public class PipeSpawnerScript : MonoBehaviour
     }
 
     float RandSpawnrate() {
-        float rate = Random.Range(spawnRateRange.x, spawnRateRange.y);
+
+        if (logic.playerScore < 10) {
+            spawnRateRange.x = 4f;
+            spawnRateRange.y = 5f;
+        } else if (logic.playerScore < 20) {
+            spawnRateRange.x = 3f;
+            spawnRateRange.y = 5f;
+        } else if (logic.playerScore < 50) {
+            spawnRateRange.x = 3f;
+            spawnRateRange.y = 4f;
+        } else {
+            spawnRateRange.x = 2f;
+            spawnRateRange.y = 4f;
+        }
+
+            float rate = Random.Range(spawnRateRange.x, spawnRateRange.y);
         return rate;
     }
 }
